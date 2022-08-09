d412c53faf36c5c6094c94445b9e18469dec7f20
Md. Latifur Reza <Reza@s3innovate.com>
2022-08-09 15:41:55 +0600

    test comm

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -8,7 +8,7 @@ namespace Core.Specifications
 {
     public interface ISpecification<T>
     {
-        Expression<Func<T,bool>> Criteria {get;}
+        Expression<Func<T,bool>> Criteria {get;} 
         List<Expression<Func<T,object>>> Includes {get;}
     }
 }
