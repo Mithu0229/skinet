d5f09bc0d95947025c2b544e46cd3e0d105fcbd8
Mithu0229 <showkat131080@gmail.com>
2022-08-20 15:24:11 +0600

    end of section 11 error panding

---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router:Router, private toastr:ToastrService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError(error=>{
+        if(error){
+          if(error.status===404){
+            this.router.navigateByUrl('/not-found');
+            this.toastr.error(error.error.message,error.error.statusCode);
+          }
+          if(error.status===500){
+            this.router.navigateByUrl('/server-error');
+          }
+        }
+        return throwError(error);
+      })
+    );
+  }
+}
